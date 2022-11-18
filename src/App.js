192d9f224e17ccab30ff4b406e7e36711d683382
Greg Stickel <gregory.stickel@live.com>
2022-11-18 16:02:10 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+
+  const [jokes, setJokes] = useState("");
+
+  const getData = () => {
+    axios
+      .get(
+        `https://api.chucknorris.io/jokes/random`
+      )
+      .then((res) => {
+        console.log(res.data);
+        setJokes(res.data);
+      });
+  };
+  
+  useEffect(() => {
+    getData();
+  }, []);
+
+  console.log("Jokes:", jokes)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header><h1>Chuck Norris Facts</h1></header>
+      <img src="https://avatars.githubusercontent.com/u/17794049?s=280&v=4" alt="" />
+      <h2><q>{jokes.value}</q></h2>
+      <button onClick={getData}>Get Another Fact</button>
     </div>
   );
 }
